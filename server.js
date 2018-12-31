20e7ff94ca7377845970f8d183cbd253bd2a525b
smit mirani <smitmirani98@gmail.com>
2018-12-31 03:03:55 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs=require('fs');
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use((req,res,next)=>{
+  res.render('maintainence.hbs');
+});
+app.use(express.static(__dirname + '/public'));
+app.use((req,res,next)=>{
+  var now=new Date().toString();
+  var log=`${now} : ${req.method} ${req.url}`;
+  console.log(log);
+   fs.appendFile('server.log',log + '\n',(err)=>{
+  if(err){
+    console.log('error occured');
+  }
+});
+  next();
+});
+// app.use((req,res,next)=>{
+//   res.render('maintainence.hbs');
+// });
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
